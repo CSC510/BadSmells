afa3001f6aa0135c1a99db533cb544bca79990b2
Jesse <jesse.xu.20@gmail.com>
2015-04-25 21:38:51 -0400

    add milestone and pull request in gitable

--- gitable.py
+++ gitable.py
@@ -47,6 +47,8 @@ class L():
 
   
 def secs(d0):
+  if d0==None:
+      return d0;
   d     = datetime.datetime(*map(int, re.split('[^\d]', d0)[:-1]))
   epoch = datetime.datetime.utcfromtimestamp(0)
   delta = d - epoch
@@ -74,6 +76,77 @@ def dump2(u, commits):
     commits[author] = all_commits
   return True
 
+def dump3(u, milestones):
+    token = "INSERT TOKEN HERE" # <===
+    request = urllib2.Request(u, headers={"Authorization" : "token "+token})
+    v = urllib2.urlopen(request).read()
+    milestone = json.loads(v)
+    if not milestone: return False
+    title = milestone['title']
+    open_issues = milestone['open_issues']
+    closed_issues = milestone['closed_issues']
+    created_at = milestone['created_at']
+    updated_at = milestone['updated_at']
+    closed_at = milestone['closed_at']
+    if closed_at != None:
+        duration=secs(closed_at)-secs(created_at)
+    else:
+         duration = inf
+    due_on = milestone['due_on']
+    milestoneObj = L( what = title,
+                   open_issues= open_issues,
+                   closed_issues =closed_issues,
+                   created_at = secs(created_at),
+                   closed_at = secs(closed_at),
+                   duration = duration/3600,
+                   due_on = secs(due_on),
+                   total_issues= int(closed_issues)+int(open_issues)
+                      )
+    all_milestones =  milestones.get(title)
+    if not all_milestones:
+        all_milestones = []
+    all_milestones.append(milestoneObj)
+    milestones[title] = all_milestones
+    return True
+    
+def dump4(u, pulls):
+  token = "INSERT TOKEN HERE" # <===
+  request = urllib2.Request(u, headers={"Authorization" : "token "+token})
+  v = urllib2.urlopen(request).read()
+  w = json.loads(v)
+  if not w: return False
+  for pull in w:
+    title = pull['title']
+    number = pull['number']
+    created_at = pull['created_at']
+    closed_at = pull['closed_at']
+    merged_at = pull['merged_at']
+    if merged_at != None:
+        process_duration=secs(merged_at)-secs(created_at)
+    elif closed_at != None:
+        process_duration=secs(closed_at)-secs(created_at)
+    else:
+        process_duration=inf
+    request2 = urllib2.Request('https://api.github.com/repos/CSC510/SQLvsNOSQL/pulls/'+str(number), headers={"Authorization" : "token "+token})
+    v2= urllib2.urlopen(request2).read()
+    w2 = json.loads(v2)
+    mergeable = w2['mergeable']
+    changed_files= w2['changed_files']
+    pullObj = L( what = title,
+                 created_at = secs(created_at),
+                 closed_at = secs(closed_at),
+                 merged_at =secs(merged_at),
+                 mergeable =mergeable,
+                 process_duration = process_duration,
+                 changed_files=changed_files
+                      )
+    print (pullObj.show())
+    all_pulls =  pulls.get(title)
+    if not all_pulls:
+        all_pulls = []
+    all_pulls.append(pullObj)
+    pulls[title] = all_pulls
+  return True
 def dumpC(u,commits):
     try:
        return  dump2(u, commits)
@@ -81,7 +154,20 @@ def dumpC(u,commits):
         print("problem when dump commits")
         print(e)
         return False
-        
+def dumpM(u,milestones):
+    try:
+        return dump3(u,milestones)
+    except Exception as e:
+        print("problem when dump milestones")
+        print(e)
+        return False;
+def dumpP(u,pulls):
+    try:
+        return dump4(u,pulls)
+    except Exception as e:
+        print("problem when dump pull requests")
+        print(e)
+        return False;
 def dump1(u,issues):
   token = "INSERT TOKEN HERE" 
   request = urllib2.Request(u, headers={"Authorization" : "token "+token})
@@ -111,8 +197,8 @@ def dump1(u,issues):
                     comments =issue['comments'],
                      #add more attributes here: attr_name = issue['attr'] , json response described here:https://developer.github.com/v3/issues/events/
                     created_at = secs(issue['created_at']),
-                    closed_at = secs(issue['closed_at'])
-                 
+                    closed_at = secs(issue['closed_at']),
+                    #duration = closed_at- created_at
                    )
       all_events.append(issueObj)          
     all_events.append(eventObj)
@@ -155,7 +241,31 @@ def dumpCommits():
         for commit in commits: print(commit.show())
         print('')
 
-dumpCommits()
+def dumpMilestones():
+    page=1
+    milestones=dict()
+    while(True):
+        doNext=dumpM('https://api.github.com/repos/CSC510/SQLvsNOSQL/milestones/'+str(page), milestones)
+        print("page "+str(page))
+        page+=1
+        if not doNext :break
+    for title,milestone in milestones.iteritems():
+        for item in milestone:
+            print (item.show())
+        print('')
+        
+def dumpPulls():
+    page=1
+    pulls=dict()
+    doNext=dumpP('https://api.github.com/repos/CSC510/SQLvsNOSQL/pulls?state=all', pulls)
+    # for author, pulls in pulls.iteritems():
+    #     print("AUTHOR "+ author)
+    #     # print(len(commits))
+    #     # for commit in commits: print(commit.show())
+    #     print('')
+dumpPulls()
+# dumpMilestones();
+# dumpCommits()
 #launchDump()
 
   
